a4f3384593e95a1a453fd0bba920d891b77b51a2
Hugo <vandenbogaert.hugo@gmail.com>
2023-04-25 16:17:55 +0200

    boite à moustache du niveau de gris

--- code/Matlab/MustachBox.m
+++ code/Matlab/MustachBox.m
@@ -12,6 +12,7 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     apple{n} = img;
     imshow(img,[]); 
+    NGapple(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/apricot/');
 list=dir([rep '*.JPG']);
@@ -21,6 +22,7 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     apricot{n} = img;
     imshow(img,[]); 
+    NGapricot(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/banana/');
 list=dir([rep '*.JPG']);
@@ -30,6 +32,7 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     banana{n} = img;
     imshow(img,[]); 
+    NGbanana(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/blueberry/');
 list=dir([rep '*.JPG']);
@@ -38,7 +41,8 @@ figure
 for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     blueberry{n} = img;
-    imshow(img,[]); 
+    %imshow(img,[]); 
+    NGblueberry(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/grape/');
 list=dir([rep '*.JPG']);
@@ -48,6 +52,7 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     grape{n} = img;
     imshow(img,[]); 
+    NGgrape(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/kiwi/');
 list=dir([rep '*.JPG']);
@@ -56,7 +61,8 @@ figure
 for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     kiwi{n} = img;
-    imshow(img,[]); 
+    imshow(img,[]);
+    NGkiwi(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/orange/');
 list=dir([rep '*.JPG']);
@@ -66,6 +72,7 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     orange{n} = img;
     imshow(img,[]); 
+    NGorange(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/pear/');
 list=dir([rep '*.JPG']);
@@ -75,6 +82,7 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     pear{n} = img;
     imshow(img,[]); 
+    NGpear(n) = mean(im2gray(img),'all');
 end
 rep = sprintf('../../images/strawberry/');
 list=dir([rep '*.JPG']);
@@ -84,4 +92,22 @@ for n = 1:nbIm
     img = double(imread(sprintf('%s%s',rep,list(n).name)))/255;
     strawberry{n} = img;
     imshow(img,[]); 
+    NGstrawberry(n) = mean(im2gray(img),'all');
 end
+moustaches = NaN(538,9);
+moustaches(1:size(NGapple,2),1) = NGapple;
+moustaches(1:size(NGapricot,2),2) = NGapricot;
+moustaches(1:size(NGbanana,2),3) = NGbanana;
+moustaches(1:size(NGblueberry,2),4) = NGblueberry;
+moustaches(1:size(NGgrape,2),5) = NGgrape; 
+moustaches(1:size(NGkiwi,2),6) = NGkiwi;
+moustaches(1:size(NGpear,2),7) = NGpear;
+moustaches(1:size(NGorange,2),8) = NGorange;
+moustaches(1:size(NGstrawberry,2),9) = NGstrawberry;
+boxplot(moustaches)
+
+
+
+
+
+
